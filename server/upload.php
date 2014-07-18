e3634b3cdd4c61e329b56a7041e1ae094dc47965
864907600cc <h@ccloli.com>
2014-07-18 16:19:43 +0800

    update README & post server file
    update README & post server file

---
+++ server/upload.php
@@ -0,0 +1,27 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('X-XSS-Proctection: 0');
+header('Content-Security-Policy: reflected-xss allow');
+header('X-Content-Security-Policy: reflected-xss allow');
+if(empty($_REQUEST['imgdata'])==false){
+	preg_match('/^data:.*?;base64,(.*)/',$_REQUEST['imgdata'],$imgdata);
+	$time=time();
+	$filename=$time.'.jpg';
+	file_put_contents($filename,base64_decode($imgdata[1]));
+	echo 'http://fh13121a.bget.ru/img/'.$filename;
+	$data=explode(' ',file_get_contents('data.txt'));
+	$new_data='';
+	$num=count($data);
+	for($i=0;$i<$num;$i++){
+		if($time-$data[$i]>300){
+			unlink($data[$i].'.jpg');
+		}
+		else{
+			$new_data.=$data[$i].' ';
+		}
+	}
+	$new_data.=$time;
+	file_put_contents('data.txt',$new_data);
+}
+exit;
+?>
