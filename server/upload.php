49780dea5b2493ca6c692f8821c81c6ffef9720e
ccloli <h@ccloli.com>
2018-09-13 12:27:05 +0800

    use uniqid() to generate file name, allow env config to change path

--- server/upload.php
+++ server/upload.php
@@ -1,26 +1,38 @@
 <?php
+error_reporting(0);
+
 header('Access-Control-Allow-Origin: *');
-header('X-XSS-Proctection: 0');
-header('Content-Security-Policy: reflected-xss allow');
-header('X-Content-Security-Policy: reflected-xss allow');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Content-Type: text/plain');
+
+$datafile=empty($_ENV['SBI_DATAFILE'])?'data.txt':$_ENV['SBI_DATAFILE'];
+$path=empty($_ENV['SBI_PREFIX'])?'http://'.$_SERVER['HTTP_HOST'].'/img/':$_ENV['SBI_PREFIX'];
+
 if(empty($_REQUEST['imgdata'])==false){
-	preg_match('/^data:.*?;base64,(.*)/',$_REQUEST['imgdata'],$imgdata);
+	preg_match('/^data:(.*?);base64,(.*)/',$_REQUEST['imgdata'],$imgdata);
 	$time=time();
-	$filename=$time.'.jpg';
-	file_put_contents($filename,base64_decode($imgdata[1]));
-	echo 'http://sbi.ccloli.com/img/'.$filename;
-	$data=explode(' ',file_get_contents('data.txt'));
+	$timestr=dechex($time);
+	$ext=explode('/',$imgdata[1])[1];
+	if(empty($ext)){
+		$ext='jpg';
+	}
+	$filename=strlen($timestr).$timestr.uniqid().'.'.$ext;
+	file_put_contents($filename,base64_decode($imgdata[2]));
+	echo $path.$filename;
+	$data=explode(' ',file_get_contents($datafile));
 	$new_data='';
 	$num=count($data);
-	for($i=0;$i<$num;$i++){
-		if($time-$data[$i]>300){
-			unlink($data[$i].'.jpg');
+	foreach($data as $item) {
+		$tlen=substr($item,0,1);
+		$t=hexdec(substr($item,1,(int)$tlen));
+		if($time-$t>300){
+			unlink($item);
 		}
 		else{
-			$new_data.=$data[$i].' ';
+			$new_data.=$item.' ';
 		}
 	}
-	$new_data.=$time;
+	$new_data.=$filename;
 	file_put_contents('data.txt',$new_data);
 }
 exit;
